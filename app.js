25683e98e69db1fc927044045acbd90ae45d8085
FelipeRocha <rochadeandradef@gmail.com>
2026-05-31 19:55:02 -0300

    criação  da função para chamar a section corresspondente ao link

--- app.js
+++ app.js
@@ -2,5 +2,18 @@ const buttons = document.querySelector("#pai-buttons")
 
 buttons.addEventListener('click', (evento) =>{
     evento.preventDefault();
-    console.log(evento.target.id)
+    
+    const puxarhref = evento.target.getAttribute('href')
+    const secaoId = document.querySelector(puxarhref)
+    if(secaoId){
+        const desativasecao = document.querySelector('.ativo')
+        if(desativasecao){
+            desativasecao.classList.remove('ativo')
+            desativasecao.classList.add('card')
+        }
+        secaoId.classList.remove('card')
+        secaoId.classList.add('ativo')
+        }
+
 })
+
