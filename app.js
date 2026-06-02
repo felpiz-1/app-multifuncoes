d28bf0515eae3ffb9dcce8e75ccf5884bf19097d
felpiz-1 <rochadeandradef@gmail.com>
2026-06-01 22:15:59 -0300

    iniciar a ferramenta de imc tentando ppegar genero

--- app.js
+++ app.js
@@ -6,14 +6,32 @@ buttons.addEventListener('click', (evento) =>{
     const puxarhref = evento.target.getAttribute('href')
     const secaoId = document.querySelector(puxarhref)
     if(secaoId){
+        if(secaoId.classList.contains('.ativo')){
+            secaoId.classList.remove('ativo')
+            secaoId.classList.add('card')
+        }
         const desativasecao = document.querySelector('.ativo')
         if(desativasecao){
             desativasecao.classList.remove('ativo')
             desativasecao.classList.add('card')
         }
         secaoId.classList.remove('card')
-        secaoId.classList.add('ativo')
+        secaoId.classList.add("ativo")
         }
 
 })
 
+const elementoImc = {
+    genero: document.querySelectorAll('.genero'),
+    form: document.getElementById('form')
+}
+     console.log(elementoImc.genero)
+
+elementoImc.genero.addEventListener('click', (evento) => {
+    puxarGenero
+})
+
+elementoImc.form.addEventListener('submit', (evento) => {
+    evento.preventDefault()
+    console.log(elementoImc.genero.value)
+})
